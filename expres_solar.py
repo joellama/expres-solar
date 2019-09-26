ee085553526dc1a9e3f067164d7426fd54264888
joellama <joellama@gmail.com>
2019-09-26 15:20:11 -0700

    updated timezones

--- expres_solar.py
+++ expres_solar.py
@@ -132,7 +132,7 @@ class expres_solar():
  
     def get_sun_for_whole_day(self):
         tnow = datetime.now().date()
-        today = datetime(tnow.year, tnow.month, tnow.day, 0, 0, 0)
+        today = datetime(tnow.year, tnow.month, tnow.day, 0, 0, 0, tzinfo=self.tz)
         tomorrow = datetime(today.year, today.month, today.day, 23, 59, 59, tzinfo=self.tz)
         time_arr = pd.date_range(start=today, end=tomorrow, freq='1min')
         frame = AltAz(obstime=time_arr, location=self.site)
